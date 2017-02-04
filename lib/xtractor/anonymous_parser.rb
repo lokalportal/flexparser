362a29a7e71dbb2c247a7adeb5a783efb89afd85
Paul Martensen <paul.martensen@gmx.de>
2017-02-04 16:34:01 +0100

    Added better debug info to anonymous_parser:

--- lib/xtractor/anonymous_parser.rb
+++ lib/xtractor/anonymous_parser.rb
@@ -5,5 +5,16 @@ module Xtractor
   #
   class AnonymousParser
     extend ClassMethods
+
+    def to_s
+      return super if self.class.parsers.empty?
+      to_h.to_s
+    end
+
+    def to_h
+      self.class.parsers.each_with_object({}) do |parser, hash|
+        hash[parser.name.to_sym] = public_send(parser.name)
+      end
+    end
   end
 end
