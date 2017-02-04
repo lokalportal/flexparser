ebc784816e8f9066f0dec8811ca71a55f7ab2a5f
Paul Martensen <paul.martensen@gmx.de>
2017-02-04 16:33:13 +0100

    Moved Xtractor-Test out of test/xtractor to test/extractor/class_methods_test.rb

---
+++ test/xtractor/class_methods_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+module Xtractor
+  class ClassMethodsTest < Minitest::Test
+    def xml
+      <<-XML
+      <bookstore>
+        <book>
+          <title>The Witcher</title>
+          <isbn>10</isbn>
+        </book>
+        <book>
+          <title>Lord of the Rings</title>
+          <isbn>11</isbn>
+        </book>
+        <book>
+          <title>Harry Potter</title>
+          <isbn>12</isbn>
+        </book>
+        <category>Fantasy</category>
+      </bookstore>
+      XML
+    end
+
+    def parser
+      Class.new do
+        include ::Xtractor
+
+        node 'bookstore' do
+          node 'category'
+          collection 'book' do
+            node 'title'
+            node 'isbn', name: :id, transform: :to_i
+          end
+        end
+      end
+    end
+
+    def feed
+      parser.parse xml
+    end
+
+    def test_inherits_anonymous
+      assert(feed.bookstore.class < Xtractor::AnonymousParser)
+    end
+
+    def test_defines_accessors
+      assert feed.bookstore.respond_to?(:category)
+      assert feed.bookstore.respond_to?('category=')
+    end
+
+    def test_parses_bookstore_correctly
+      assert_equal feed.bookstore.category, 'Fantasy'
+    end
+
+    def test_collection_parsing
+      assert_equal feed.bookstore.book.count, 3
+      assert_equal feed.bookstore.book.first.title, 'The Witcher'
+    end
+
+    def test_name_and_transform_option
+      assert_equal feed.bookstore.book.first.id, 10
+    end
+  end
+end
