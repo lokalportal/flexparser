9298db9d028f108b5188120cc48e2a4decfedd42
Paul Martensen <paul.martensen@gmx.de>
2017-07-26 13:59:19 +0200

    Added option to set configuration back to default.

--- lib/xtractor/configuration.rb
+++ lib/xtractor/configuration.rb
@@ -8,12 +8,16 @@ module Xtractor
     attr_accessor(*AVAILABLE_OPTIONS)
 
     def initialize
-      @rety_without_namespaces  = true
-      @explicit_property_naming = true
+      set_defaults
     end
 
     def to_h
       Hash[AVAILABLE_OPTIONS.map { |o| [o, public_send(o)] }]
+    end
+
+    def set_defaults
+      @rety_without_namespaces  = true
+      @explicit_property_naming = true
     end
   end
 end
